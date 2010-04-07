177d4e6b7c99475cd03001f851180ce1f1d99d34
chriswhite124 <chris.white@medialets.com>
2010-04-06 22:34:14 -0400

    initial commit

---
+++ src/com/medialets/funnyquotes/funnyquotes.java
@@ -0,0 +1,18 @@
+package com.medialets.funnyquotes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class funnyquotes extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    public void foo() {
+        int x = 3;
+        x = x;
+      }
+}
